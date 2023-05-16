9bcf8ad1cd9ac35c4fd56d1a9e0d63f44e241b0d
Brian Bernal <brianbernal27@gmail.com>
2023-05-16 12:03:40 -0500

    chore(api): set up initial boilerplate

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+// *** ADD ***
+const v1Router = require("./v1/routes");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// *** REMOVE ***
+app.get("/", (req, res) => {
+  res.send("<h2>It's Working!</h2>");
+});
+
+// *** ADD ***
+app.use("/api/v1", v1Router);
+
+app.listen(PORT, () => {
+  console.log(`API is listening on port ${PORT}`);
+});
