5e8ce667a87bbf4f64edb0fe2eb16bcf2a2f7b8e
Brian Bernal <brianbernal27@gmail.com>
2023-05-16 18:48:39 -0500

    feat: remove unnecessary header

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -6,19 +6,9 @@ import "./layout.scss";
 
 function Layout() {
   return (
-    <>
-      <header className="header">
-        <nav className="container">
-          <div className="profile-badge">
-            <img src="profile-image.jpg" alt="Profile Image" />
-            <span className="username">John Doe</span>
-          </div>
-        </nav>
-      </header>
-      <main className="container">
-        <Outlet />
-      </main>
-    </>
+    <main className="container">
+      <Outlet />
+    </main>
   );
 }
 
